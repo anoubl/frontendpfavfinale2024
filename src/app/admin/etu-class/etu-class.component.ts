882365d535f1c48f585f5325b1eaf47c7444e9cc
Achrafgab10 <achrafgab3@gmail.com>
2024-06-16 23:36:03 +0200

    wweeeee

---
+++ src/app/admin/etu-class/etu-class.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NavComponent} from "../nav/nav.component";
+
+@Component({
+  selector: 'app-etu-class',
+  standalone: true,
+  imports: [
+    NavComponent
+  ],
+  templateUrl: './etu-class.component.html',
+  styleUrl: './etu-class.component.css'
+})
+export class EtuClassComponent {
+
+}
